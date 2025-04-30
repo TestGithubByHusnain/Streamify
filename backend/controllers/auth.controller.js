93a5de411090066b096b2c8c7a0a742b04140175
Husnain <husnainali41940@gmail.com>
2025-04-30 02:04:53 -0700

    Initial commit with frontend and backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+
+export async function signUp(req,res) {
+    res.send("Signup route")
+}
+export async function login(req,res) {
+    res.send("Login route")
+}
+export async function logout(req,res) {
+    res.send("Logout route")
+}
